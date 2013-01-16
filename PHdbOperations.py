75ee75ad5b3edc846588f98529170abe7f1b07b7
CJOlsen <co_devaccount@yahoo.com>
2013-01-16 14:54:57 -0700

    first commit

---
+++ PHdbOperations.py
@@ -0,0 +1,355 @@
+## Payable Hours
+## Python 2.7
+## Christopher Olsen
+
+# Copyright Notice: Copyright 2012 Christopher Olsen
+# License: GNU General Public License, v3 (see LICENSE.txt)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import MySQLdb as mdb
+import sys
+
+# globals 
+connection = None
+cursor = None
+
+# general database operations
+def connectDB():
+    """ Takes: nothing
+        Tries to create a connection to the MySQL database.
+        Returns: connection, cursor (which are None if the con fails)
+        """
+    global connection, cursor
+    try:
+        connection = mdb.connect('localhost',
+                                 'james',
+                                 'password',
+                                 'PayableHours');
+
+        cursor = connection.cursor()
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        sys.exit(1)
+    return connection, cursor
+
+def disconnectDB():
+    """ Takes: nothing
+        Disconnects from the database.
+        Returns: nothing
+        """
+    if connection:
+        connection.commit()
+        connection.close()
+
+def getAllCompanies():
+    """ Takes: nothing
+        Selects all names from the company table
+        Returns: all company names
+        """
+    cursor.execute("SELECT name FROM company")
+    return [x[0] for x in cursor.fetchall()]
+    
+def getCompanyByName(name):
+    """ Takes: a name string
+        Returns: a company object corresponding to the name
+        """
+    company = Company()
+    return company.getByName(name)
+    
+def getAllProjects():
+    """ Takes: nothing
+        Returns: all names from the project table
+        """
+    cursor.execute("SELECT name FROM project")
+    return cursor.fetchall()
+
+def getActiveProjects():
+    """ Takes: nothing
+        Returns: all names from the project table if the project is active
+        """
+    cursor.execute("SELECT name FROM project WHERE projectActive = True")
+    return cursor.fetchall()
+
+def getAllContacts():
+    """ Takes: nothing
+        Returns: all names from the contact table
+        """
+    cursor.execute("SELECT name FROM contact")
+    return cursor.fetchall()
+
+def getContactsForCompany(name):
+    # to do
+    pass
+
+def getContactByID():
+    # to do
+    pass
+    
+
+
+    
+# objects correspond to relations (this is a simple ORM system)
+class Company(object):
+    def __init__(self, 
+                 companyID =None,
+                 name      =None,
+                 address   =None,
+                 city      =None,
+                 state     =None,
+                 phone     =None,
+                 notes     =None):
+        self.companyID = companyID
+        self.name      = name
+        self.address   = address
+        self.city      = city
+        self.state     = state
+        self.phone     = phone
+        self.notes     = notes
+
+    # because Python doesn't allow multiple init's it's easier to initialize
+    # a new object in two steps than mangle __init__, but getByName really is
+    # part of the initialization process
+    def getByName(self,name):
+        """ Takes: a name string (and implied self)
+            Queries the database for the company corresponding to the name
+            string, and maps attributes ot local variables.
+            Returns: self
+            """
+        global cursor
+        cursor = connection.cursor(mdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM company WHERE name=%s",(name))
+        recordDict = cursor.fetchall()[0]
+        
+        if recordDict:
+            self.companyID = recordDict['id']
+            self.name      = recordDict['name']
+            self.address   = recordDict['address']
+            self.city      = recordDict['city']
+            self.state     = recordDict['state']
+            self.phone     = recordDict['phone']
+            self.notes     = recordDict['notes']
+        else:
+            print 'there was an error'
+            
+        cursor = connection.cursor()
+        return self
+    
+    def write(self):
+        """ Takes: nothing (implied self)
+            Writes all data in the company object to the database, and commits
+            Returns: Nothing
+            """
+        # this is dangerous, there is no undo
+        global cursor
+        names = getAllCompanies()
+        if self.name in names:
+            cursor.execute("SELECT id FROM company WHERE name='%s'"\
+                           % self.name)
+            self.companyID = cursor.fetchone()[0]
+            cursor.execute("REPLACE INTO company (id, name, address, city, state, phone, notes)"\
+                           "VALUES (%s,%s,%s,%s,%s,%s,%s)",\
+                           (self.companyID,self.name,self.address,self.city,\
+                            self.state,self.phone,self.notes))
+        else:
+            cursor.execute("INSERT INTO company (name, address, city, state, phone, notes)"\
+                           "VALUES (%s,%s,%s,%s,%s,%s)",\
+                           (self.name,self.address,self.city,self.state,\
+                            self.phone,self.notes))
+        connection.commit()
+
+class Contact(object):
+    def __init__(self,
+                 name      =None,
+                 contactID =None,
+                 companyID =None,
+                 phone     =None,
+                 email     =None,
+                 notes     =None):
+        self.name      = name
+        self.contactID = contactID
+        self.companyID = companyID
+        self.phone     = phone
+        self.email     = email
+        self.notes     = notes
+
+    def write(self):
+        print 'write'
+        if self.contactID:
+            cursor.execute("REPLACE INTO contact (%s,%s,%s,%s,%s,%s)",\
+                           (self.contactID,self.companyID,self.name,self.phone,\
+                            self.email,self.notes))
+        else:
+            cursor.execute("INSERT INTO project"\
+                           "(companyID, name, phone, email, notes)\
+                           (%s,%s,%s,%s,%s)",\
+                           (self.companyID,self.name,self.phone,self.email,\
+                            self.notes))
+            
+        connection.commit()
+
+    def getByName(self,name):
+        cursor = connection.cursor(mdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM contact WHERE name=%s",(name))
+        recordDict = cursor.fetchall()[0]
+        
+        if recordDict:
+            self.contactID = recordDict['contactID']
+            self.companyID = recordDict['companyID']
+            self.name      = recordDict['name']
+            self.email     = recordDict['email']
+            self.notes     = recordDict['notes']
+            self.phone     = recordDict['phone']
+        else:
+            print 'there was an error'
+    
+        
+
+
+class Project(object):
+    def __init__(self, 
+                 name          =None,
+                 companyID     =None,
+                 hourlyPay     =None,
+                 quotedHours   =None,
+                 workedHours   =None,
+                 billedHours   =None,
+                 totalInvoiced =None,
+                 totalPaid     =None,
+                 moneyOwed     =None,
+                 projectActive =None,
+                 contactName   =None,
+                 contactPhone  =None,
+                 notes         =None):
+        self.name          = name
+        self.companyID     = companyID
+        self.hourlyPay     = hourlyPay
+        self.quotedHours   = quotedHours
+        self.workedHours   = workedHours
+        self.billedHours   = billedHours
+        self.totalInvoiced = totalInvoiced
+        self.totalPaid     = totalPaid
+        self.moneyOwed     = moneyOwed
+        self.projectActive = projectActive
+        self.contactName   = contactName
+        self.contactPhone  = contactPhone
+        self.notes         = notes
+            
+    def write():
+        # query to write to DB
+        if self.companyID:
+            # companyID should only be assigned by MySQl, so if an object
+            # has one, it already exists and needs to be overwritten
+            # REPLACE will overwrite or add new, INSERT adds new only
+            cursor.execute("REPLACE INTO project\
+                           (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",\
+                           (self.name,self.companyID,self.hourlyPay,\
+                            self.quotedHours,self.workedHours,self.billedHours,\
+                            self.totalInvoiced,self.totalPaid,self.moneyOwed,\
+                            self.projectActive,self.contactName,\
+                            self.contactPhone,self.notes))
+        else:
+            cursor.execute("INSERT INTO project\
+                           (name, address, city, state, phone, notes)\
+                           (%s,%s,%s,%s,%s,%s)",\
+                           (self.name,self.address,self.city,self.state,\
+                            self.phone,self.notes))
+
+        # nothing is final until committed    
+        connection.commit()
+
+
+    def getRecordByName(self,name):
+        cursor = connection.cursor(mdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM project WHERE name=%s",(name))
+        recordDict = cursor.fetchall()[0]
+        
+        if recordDict:
+            self.name          = recordDict['name']
+            self.companyID     = recordDict['companyID']
+            self.hourlyPay     = recordDict['hourlyPay']
+            self.quotedHours   = recordDict['quotedHours']
+            self.workedHours   = recordDict['workedHours']
+            self.billedHours   = recordDict['billedHours']
+            self.totalInvoiced = recordDict['totalInvoiced']
+            self.totalPaid     = recordDict['totalPaid']
+            self.moneyOwed     = recordDict['moneyOwed']
+            self.projectActive = recordDict['projectActive']
+            self.contactName   = recordDict['contactName']
+            self.contactPhone  = recordDict['contactPhone']
+            self.notes         = recordDict['notes']
+        else:
+            print 'there was an error'
+            
+        cursor = connection.cursor()
+        return self
+
+    
+class Session(object):
+    def __init__(self, 
+                 sessionid =None,
+                 companyID =None,
+                 projectID =None,
+                 startTime =None,
+                 stopTime  =None,
+                 time      =None,
+                 notes     =None):
+        self.sessionID = sessionID
+        self.companyID = companyID
+        self.projectID = projectID
+        self.startTime = startTime
+        self.stopTime  = stopTime
+        self.time      = time
+        self.notes     = notes
+
+
+    def write():
+        # query to write to DB
+        if self.sessionid:
+            #sessionID should only be assigned by MySQl, so if an object
+            #has one, it already exists and needs to be overwritten
+            cursor.execute("REPLACE INTO session\
+                           (%s,%s,%s,%s,%s,%s,%s)",\
+                           (self.sessionID,self.companyID,self.projectID,\
+                            self.startTime,self.stopTime,self.time,self.notes))
+        else:
+            cursor.execute("INSERT INTO session\
+                           (companyID, projectID, startTime, stopTime,\
+                           time,notes)(%s,%s,%s,%s,%s,%s)",\
+                           (self.companyID,self.projectID,\
+                            self.startTime,self.stopTime,self.time,self.notes))
+            
+        connection.commit()
+
+    def getRecordByName(self,name):
+        cursor = connection.cursor(mdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM session WHERE name=%s",(name))
+        recordDict = cursor.fetchall()[0]
+        
+        if recordDict:
+            self.name      = recordDict['name']
+            self.sessionID = recordDict['sessionID']
+            self.companyID = recordDict['companyID']
+            self.projectID = recordDict['projectID']
+            self.startTime = recordDict['startTime']
+            self.stopTime  = recordDict['stopTime']
+            self.time      = recordDict['time']
+            self.notes     = recordDict['notes']
+
+        else:
+            print 'there was an error'
+        cursor = connection.cursor()
+
---
+++ PHdisplay.py
@@ -0,0 +1,326 @@
+# Payable Hours
+# Author: Christopher Olsen
+#
+# Copyright Notice: Copyright 2012 Christopher Olsen
+# License: GNU General Public License, v3 (see LICENSE.txt)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## *** THIS FILE HOLDS THE DISPLAY CODE, FOR MYSQL INTERFACE SEE 
+##     PHdbOperations.py and for program logic see PHlogic.py ***
+
+
+import sys
+
+
+## **** workaround for importing other modules, needs fixing ****
+## 
+##if '**your filepath**/payablehours/' not in sys.path:
+##    print 'adding to sys path'
+##    sys.path.append('**your filepath**/payablehours/')
+
+
+
+import PHdbOperations as PHdb
+import PHlogic as PHlo
+import Tkinter as tk
+import ttk
+
+print PHdb.__file__
+print PHlo.__file__
+
+print type(PHdb), 'PHdb', dir(PHdb)
+print type(PHlo), 'PHlo', dir(PHlo)
+
+
+#connect to database
+connection,cursor = PHdb.connectDB()
+
+
+
+################################################################################
+#### BUILD THE DISPLAY
+################################################################################
+
+
+#create main window 
+root = tk.Tk()
+root.title("Payable Horse")
+root.geometry("600x600+100+50")
+
+#create notebook
+nbook = ttk.Notebook(root)
+nbook.grid(row=0,column=0)
+
+#make tabs and add them
+companyTab = tk.Frame()
+contactTab = tk.Frame()
+projectTab = tk.Frame()
+sessionTab = tk.Frame()
+mysqlTab   = tk.Frame()
+
+nbook.add(companyTab,text='Company')
+nbook.add(contactTab,text='Contact')
+nbook.add(projectTab,text='Project')
+nbook.add(sessionTab,text='Session')
+nbook.add(mysqlTab,text='MySQL')
+
+
+#### company page content------------------------------------------------------
+  ## set up the fields and labels
+company_name_field    = tk.Entry(companyTab,width=25)
+company_address_field = tk.Entry(companyTab,width=25)
+company_city_field    = tk.Entry(companyTab,width=25)
+company_state_field   = tk.Entry(companyTab,width=5)
+company_phone_field   = tk.Entry(companyTab,width=25)
+company_notes_field   = tk.Entry(companyTab,width=25)
+
+#company_fields = [company_name_field, company_address_field, 
+#                  company_city_field,company_state_field, 
+#                  company_phone_field,company_notes_field]
+
+company_name_label    = tk.Label(companyTab,text="Name")
+company_address_label = tk.Label(companyTab,text="Address")
+company_city_label    = tk.Label(companyTab,text="City")
+company_state_label   = tk.Label(companyTab,text="State")
+company_phone_label   = tk.Label(companyTab,text="Phone")
+company_notes_label   = tk.Label(companyTab,text="Notes")
+
+
+  ## create a listbox
+company_listbox = tk.Listbox(companyTab)
+
+for item in PHdb.getAllCompanies():
+    company_listbox.insert('end',item)
+
+  ## create a button to select from the listbox
+
+# this needs a lambda function because it stores the result of the command
+# company_selected defined in PHlogic.py
+company_listbox_button = tk.Button(companyTab, text="Select",
+                                   command = lambda: PHlo.company_selected(
+                                       company_listbox.get(
+                                           company_listbox.curselection()[0])
+                                           ,companyTab))
+
+company_save_button = tk.Button(companyTab,text='Save',
+                                command = lambda: company_save())
+
+company_clear_button = tk.Button(companyTab,text='Clear',
+                                 command = lambda: \
+                                 [field.delete(0,'end') for field in\
+                                  companyTab.winfo_children() \
+                                  if field.winfo_class() == 'Entry'])
+
+
+#### contact page content -----------------------------------------------------
+
+contact_name_field  = tk.Entry(contactTab,width=20)
+contact_phone_field = tk.Entry(contactTab,width=20)
+contact_email_field = tk.Entry(contactTab,width=20)
+contact_notes_field = tk.Entry(contactTab,width=20)
+
+contact_name_label  = tk.Label(contactTab,text="Name")
+contact_phone_label = tk.Label(contactTab,text="Phone")
+contact_email_label = tk.Label(contactTab,text="Email")
+contact_notes_label = tk.Label(contactTab,text="Notes")
+
+# company list box
+
+
+
+#### project page content -----------------------------------------------------
+
+project_hourlyPay_field     = tk.Entry(projectTab,width=25)
+project_quotedHours_field   = tk.Entry(projectTab,width=25)
+project_workedHours_field   = tk.Entry(projectTab,width=25)
+project_billedHours_field   = tk.Entry(projectTab,width=25)
+project_totalInvoiced_field = tk.Entry(projectTab,width=25)
+project_totalPaid_field     = tk.Entry(projectTab,width=25)
+project_moneyOwed_field     = tk.Entry(projectTab,width=25)
+project_projectActive_field = tk.Entry(projectTab,width=5)
+project_contactName_field   = tk.Entry(projectTab,width=25)
+project_contactPhone_field  = tk.Entry(projectTab,width=25)
+#project_notes_field         = tk.Entry(projectTab,width=200)\
+#                              .grid(row=11,column=1)
+
+project_hourlyPay_label     = tk.Label(projectTab,text="Hourly Pay")
+project_quotedHours_label   = tk.Label(projectTab,text="Quoted Hours")
+project_workedHours_label   = tk.Label(projectTab,text="Worked Hours")
+project_billedHours_label   = tk.Label(projectTab,text="Billed Hours")
+project_totalInvoiced_label = tk.Label(projectTab,text="Total Invoiced")
+project_totalPaid_label     = tk.Label(projectTab,text="Total Paid")
+project_moneyOwed_label     = tk.Label(projectTab,text="Money Owed")
+project_projectActive_label = tk.Label(projectTab,text="Project Active")
+project_contactName_label   = tk.Label(projectTab,text="Contact Name")
+project_contactPhone_label  = tk.Label(projectTab,text="Contact Phone")
+project_notes_label         = tk.Label(projectTab,text="Notes Label")
+
+#company list box
+
+#contact list box
+ ## create the listboxes
+project_company_listbox = tk.Listbox(projectTab)
+project_contact_listbox = tk.Listbox(projectTab)
+
+for item in PHdb.getAllCompanies():
+    project_company_listbox.insert('end',item)
+
+def update_project_company_listbox(name):
+    for item in PHdb.getContactsForCompany(name):
+        project_contact_listbox.insert('end',item)
+
+        
+    ## create a button to select from the company listbox
+    ## ...
+
+
+
+                              
+#### session page content -----------------------------------------------------
+
+session_startTime_field    = tk.Entry(sessionTab,width=15)
+session_stopTime_field     = tk.Entry(sessionTab,width=15)
+session_time_field         = tk.Entry(sessionTab,width=15)
+#session_notes_field        = tk.Entry(sessionTab,width=200).grid(row=3,column=1)
+
+session_startTime_label    = tk.Label(sessionTab,text="Start time")
+session_stopTime_label     = tk.Label(sessionTab,text="Stop time")
+session_time_label         = tk.Label(sessionTab,text="Time")
+session_notes_label        = tk.Label(sessionTab,text="Notes")
+
+#company list box
+
+#contact list box
+
+#project list box
+
+
+
+#### MySQL prompt
+# just a window to pull up a sql command line prompt
+# not sure if this is even possible.....
+
+
+
+################################################################################
+#### DISPLAY THE DISPLAY
+################################################################################
+
+
+  ## display the company widgets
+company_name_field.grid(row=0,column=1)
+company_address_field.grid(row=1,column=1)
+company_city_field.grid(row=2,column=1)
+company_state_field.grid(row=3,column=1,sticky='w')
+company_phone_field.grid(row=4,column=1)
+company_notes_field.grid(row=5,column=1,rowspan=4)
+
+company_name_label.grid(row=0,column=0)
+company_address_label.grid(row=1,column=0)
+company_city_label.grid(row=2,column=0)
+company_state_label.grid(row=3,column=0)
+company_phone_label.grid(row=4,column=0)
+company_notes_label.grid(row=5,column=0)
+
+company_listbox.grid(column=4,row=0,rowspan=5)
+company_listbox_button.grid(row=5,column=4)
+company_save_button.grid(row=10,column=1)
+company_clear_button.grid(row=10,column=2)
+
+  ## display the session widgets
+session_startTime_field.grid(row=0,column=1)
+session_stopTime_field.grid(row=1,column=1)
+session_time_field.grid(row=2,column=1)
+
+session_startTime_label.grid(row=0,column=0)
+session_stopTime_label.grid(row=1,column=0)
+session_time_label.grid(row=2,column=0)
+session_notes_label.grid(row=3,column=0)
+
+  ## display the contact widgets
+contact_name_field.grid(row=0,column=1)
+contact_phone_field.grid(row=1,column=1)
+contact_email_field.grid(row=2,column=1)
+contact_notes_field.grid(row=3,column=1)
+
+contact_name_label.grid(row=0,column=0)
+contact_phone_label.grid(row=1,column=0)
+contact_email_label.grid(row=2,column=0)
+contact_notes_label.grid(row=3,column=0)
+
+  ## display the project widgets
+project_hourlyPay_field.grid(row=0,column=1)
+project_quotedHours_field.grid(row=2,column=1)
+project_workedHours_field.grid(row=3,column=1)
+project_billedHours_field.grid(row=4,column=1)
+project_totalInvoiced_field.grid(row=5,column=1)
+project_totalPaid_field.grid(row=6,column=1)
+project_moneyOwed_field.grid(row=7,column=1)
+project_projectActive_field.grid(row=8,column=1)
+project_contactName_field.grid(row=9,column=1)
+project_contactPhone_field.grid(row=10,column=1)
+
+project_hourlyPay_label.grid(row=0,column=0)
+project_quotedHours_label.grid(row=2,column=0)
+project_workedHours_label.grid(row=3,column=0)
+project_billedHours_label.grid(row=4,column=0)
+project_totalInvoiced_label.grid(row=5,column=0)
+project_totalPaid_label.grid(row=6,column=0)
+project_moneyOwed_label.grid(row=7,column=0)
+project_projectActive_label.grid(row=8,column=0)
+project_contactName_label.grid(row=9,column=0)
+project_contactPhone_label.grid(row=10,column=0)
+project_notes_label.grid(row=11,column=0)
+
+## 
+## This line is so necessary.  Don't remove it.
+##
+root.mainloop()
+
+
+
+"""
+    WHAT WILL THE GUI NEED?
+        -five tabs:
+            -Company
+                - show all data
+                - select company dropdown
+                - save changes button
+            -Contact
+                - show all data
+                - select contact dropdown
+                - save changes button
+            -Project
+                - show all data
+                - select project
+                - save changes button
+                - start/stop project session
+            -Session
+                - session search feature
+                - show all data
+                - save changes button
+                - start/stop button
+                - combine sessions
+            -MySQL prompt
+                - Open a window with a MySQL command line interface
+"""
+
+    
+
+
+
+
+
